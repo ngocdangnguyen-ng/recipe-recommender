533961ccb81088edb1bfec5751d59c75019f6174
n23015330 <nndnguyen2016@gmail.com>
2025-04-15 15:51:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,18 @@ if st.button("Rechercher"):
     recommendations = recommend_recipes(df, recipe_name)
     if not recommendations.empty:
         st.write("Recettes similaires :")
-        st.write(recommendations)
+        for index, row in recommendations.iterrows():
+            with st.expander(row["name"]):
+                st.write("**Description :**", row["description"])
+                st.write("**Cuisine :**", row["cuisine"])
+                st.write("**Course :**", row["course"])
+                st.write("**Diet :**", row["diet"])
+                st.write("**Ingrédients :**", row["ingredients_name"])
+                st.write("**Quantité des ingrédients :**", row["ingredients_quantity"])
+                st.write("**Temps de préparation :**", row["prep_time (in mins)"], "minutes")
+                st.write("**Temps de cuisson :**", row["cook_time (in mins)"], "minutes")
+                st.write("**Instructions :**", row["instructions"])
+                if pd.notna(row["image_url"]):
+                    st.image(row["image_url"], caption=row["name"])
     else:
         st.warning("Aucune recette similaire trouvée ! Essayez un autre nom.")
