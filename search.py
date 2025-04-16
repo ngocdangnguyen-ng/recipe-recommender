8c11956c503b647e8700d0257a5f55ba4ca7b3cc
n23015330 <nndnguyen2016@gmail.com>
2025-04-16 14:23:56 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,11 +1,24 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd  # Assurez-vous que pandas est importé
+
+# Fonction de pagination
+def paginate(df, page_size=10):
+    total_pages = (len(df) - 1) // page_size + 1
+    page_num = st.number_input("Page", min_value=1, max_value=total_pages, value=1)
+    start = (page_num - 1) * page_size
+    end = start + page_size
+    return df.iloc[start:end]
 
 def search_by_name(df, recipe_name):
     recommendations = df[df["name"].str.contains(recipe_name, case=False, na=False)]
+
     if not recommendations.empty:
         st.write("### Recettes similaires :")
-        for index, row in recommendations.iterrows():
+
+        # Appliquer la pagination
+        paginated_recipes = paginate(recommendations)
+
+        for index, row in paginated_recipes.iterrows():
             display_recipe(row)
     else:
         st.warning("Aucune recette similaire trouvée ! Essayez un autre nom.")
@@ -14,9 +27,14 @@ def search_by_ingredients(df, ingredients):
     if ingredients:
         ingredient_list = ingredients.split(',')
         filtered_recipes = df[df['ingredients_name'].apply(lambda x: all(ingredient.strip().lower() in x.lower() for ingredient in ingredient_list))]
+
         if not filtered_recipes.empty:
             st.write("### Recettes trouvées :")
-            for index, row in filtered_recipes.iterrows():
+
+            # Appliquer la pagination
+            paginated_recipes = paginate(filtered_recipes)
+
+            for index, row in paginated_recipes.iterrows():
                 display_recipe(row)
         else:
             st.warning("Aucune recette trouvée avec ces ingrédients ! Essayez d'autres ingrédients.")
@@ -24,44 +42,47 @@ def search_by_ingredients(df, ingredients):
         st.warning("Veuillez entrer des ingrédients pour la recherche.")
 
 def search_by_category(df, category):
-    category_filters = {
-        "Easy Dinner": df["course"].str.contains("dinner", case=False, na=False),
-        "Under 30 Minutes": (df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 30,
-        "Chicken": df["ingredients_name"].str.contains("chicken", case=False, na=False),
-        "Breakfast": df["course"].str.contains("breakfast", case=False, na=False),
-        "Desserts": df["course"].str.contains("dessert", case=False, na=False)
-    }
-    filtered_recipes = df[category_filters.get(category, pd.Series([False] * len(df)))]
+    if category == "Easy Dinner":
+        filtered_recipes = df[df["course"].str.contains("dinner", case=False, na=False)]
+    elif category == "Under 30 Minutes":
+        filtered_recipes = df[(df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 30]
+    elif category == "Chicken":
+        filtered_recipes = df[df["ingredients_name"].str.contains("chicken", case=False, na=False)]
+    elif category == "Breakfast":
+        filtered_recipes = df[df["course"].str.contains("breakfast", case=False, na=False)]
+    elif category == "Desserts":
+        filtered_recipes = df[df["course"].str.contains("dessert", case=False, na=False)]
+
     if not filtered_recipes.empty:
         st.write(f"### Recettes pour la catégorie {category} :")
-        for index, row in filtered_recipes.iterrows():
+
+        # Appliquer la pagination
+        paginated_recipes = paginate(filtered_recipes)
+
+        for index, row in paginated_recipes.iterrows():
             display_recipe(row)
     else:
         st.warning(f"Aucune recette trouvée pour la catégorie {category} !")
 
-def search_by_filters(df, difficulty=None, diets=None, meal=None, cuisine=None):
-    filtered_recipes = df.copy()
-    
-    if difficulty:
-        difficulty_filters = {
-            "Under 1 Hour": (df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 60,
-            "Under 45 Minutes": (df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 45,
-            "Under 30 Minutes": (df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 30
-        }
-        filtered_recipes = filtered_recipes[difficulty_filters.get(difficulty, pd.Series([True] * len(df)))]
-    
-    if diets:
-        filtered_recipes = filtered_recipes[filtered_recipes["diet"].str.contains(diets, case=False, na=False)]
-    
-    if meal:
-        filtered_recipes = filtered_recipes[filtered_recipes["course"].str.contains(meal, case=False, na=False)]
-    
-    if cuisine:
-        filtered_recipes = filtered_recipes[filtered_recipes["cuisine"].str.contains(cuisine, case=False, na=False)]
-    
+def search_by_filters(df, difficulty, diets, meal, cuisine):
+    if difficulty == "Under 1 Hour":
+        filtered_recipes = df[(df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 60]
+    elif difficulty == "Under 45 Minutes":
+        filtered_recipes = df[(df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 45]
+    elif difficulty == "Under 30 Minutes":
+        filtered_recipes = df[(df["prep_time (in mins)"] + df["cook_time (in mins)"]) <= 30]
+
+    filtered_recipes = filtered_recipes[filtered_recipes["diet"].str.contains(diets, case=False, na=False)]
+    filtered_recipes = filtered_recipes[filtered_recipes["course"].str.contains(meal, case=False, na=False)]
+    filtered_recipes = filtered_recipes[filtered_recipes["cuisine"].str.contains(cuisine, case=False, na=False)]
+
     if not filtered_recipes.empty:
         st.write(f"### Recettes pour les filtres sélectionnés :")
-        for index, row in filtered_recipes.iterrows():
+
+        # Appliquer la pagination
+        paginated_recipes = paginate(filtered_recipes)
+
+        for index, row in paginated_recipes.iterrows():
             display_recipe(row)
     else:
         st.warning("Aucune recette trouvée pour les filtres sélectionnés !")
@@ -69,14 +90,17 @@ def search_by_filters(df, difficulty=None, diets=None, meal=None, cuisine=None):
 def display_recipe(row):
     with st.container():
         col1, col2 = st.columns([1, 3])
+
         with col1:
             if pd.notna(row["image_url"]):
-                st.image(row["image_url"], width=150)
+                st.image(row["image_url"], width=150)  # Taille de l'image ajustée
+
         with col2:
             st.subheader(row["name"])
             st.write("**Cuisine :**", row["cuisine"])
             st.write("**Temps de préparation :**", row["prep_time (in mins)"], "minutes")
             st.write("**Temps de cuisson :**", row["cook_time (in mins)"], "minutes")
+
             with st.expander("Voir tout"):
                 st.write("**Description :**", row["description"])
                 st.write("**Course :**", row["course"])
@@ -84,5 +108,6 @@ def display_recipe(row):
                 st.write("**Ingrédients :**", row["ingredients_name"])
                 st.write("**Quantité des ingrédients :**", row["ingredients_quantity"])
                 st.write("**Instructions :**", row["instructions"])
+
                 if pd.notna(row["image_url"]):
-                    st.image(row["image_url"], caption=row["name"], width=300)
+                    st.image(row["image_url"], caption=row["name"], width=300)  # Taille de l'image ajustée
