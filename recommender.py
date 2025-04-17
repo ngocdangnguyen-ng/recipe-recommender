215310cc4a4870a3fdfd0c73ae832403d235d258
n23015330 <nndnguyen2016@gmail.com>
2025-04-17 16:13:01 +0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -16,14 +16,14 @@ class RecipeRecommender:
         self.similarity_matrix = cosine_similarity(ingredient_matrix, ingredient_matrix)
 
    def get_similar_recipes(self, recipe_name, top_n=9):
-    if recipe_name not in self.data['name'].values:
-        return pd.DataFrame()
-
-    idx = self.data.index[self.data['name'] == recipe_name][0]
-    similarity_scores = list(enumerate(self.similarity_matrix[idx]))
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-
-    # Exclure la recette elle-même (indice 0)
-    similar_indices = [i for i, _ in similarity_scores[1:top_n+1]]
-    return self.data.iloc[similar_indices][['name', 'image_url', 'prep_time (in mins)', 'cook_time (in mins)', 
-                                            'cuisine', 'description', 'course', 'diet', 'ingredients_name', 'ingredients_quantity', 'instructions']]
+        if recipe_name not in self.data['name'].values:
+            return pd.DataFrame()
+    
+        idx = self.data.index[self.data['name'] == recipe_name][0]
+        similarity_scores = list(enumerate(self.similarity_matrix[idx]))
+        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    
+        # Exclure la recette elle-même (indice 0)
+        similar_indices = [i for i, _ in similarity_scores[1:top_n+1]]
+        return self.data.iloc[similar_indices][['name', 'image_url', 'prep_time (in mins)', 'cook_time (in mins)', 
+                                                'cuisine', 'description', 'course', 'diet', 'ingredients_name', 'ingredients_quantity', 'instructions']]
